87ebb76102719f11a0cca01f4189e60110dc25eb
Josh Daniels <82977701+CodingComp@users.noreply.github.com>
2024-07-19 23:28:20 -0600

    Start to light detection
    A working version of a enemy light detection system has been made. This system will be used for enemies in the game. When an enemy is illuminated by light they will take damage. Currently the testing spheres display when they are or aren't in light based off their color.
    
    This system works as follows. Once a gameobject enters the camera / is visible it gets added to a dictionary. For each visible enemy a bounding box of rays are casted from the camera. If a ray hits the enemy gameobject it then checks the pixels color value, (pixel from the render texture i.e lower res pixel size). If the brightness is higher than a threshold the enemy is in light. Performance is ok for the time being. 10 enemies can be on screen with 80+ fps. I will keep making performance improvements where possible.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Jobs.LowLevel.Unsafe;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -11,6 +14,7 @@ public class Player : MonoBehaviour
     public Transform orientation;
 
     public LayerMask groundMask;
+    public LayerMask enemyMask;
     private Vector3 moveDir;
     public float moveSpeed = 10.0f;
     
@@ -26,6 +30,14 @@ public class Player : MonoBehaviour
     private float verticalInput;
 
     public GameObject flashlight;
+
+    private Vector3 camWorldBoundry;
+    private float pixelSize;
+    
+    private Dictionary<GameObject, LightTest> visibleEnemies;
+
+    private int rayPaddingX = 10;
+    private int rayPaddingY = 10;
     
     void Start()
     {
@@ -40,14 +52,21 @@ public class Player : MonoBehaviour
         // RENDER TEXTURE RESOLUTION: 640 x 360
         resScaleX = (float)resX / Screen.width;
         resScaleY = (float)resY / Screen.height;
+
+        // Pixel Testing
+        camWorldBoundry = new Vector2(camera.orthographicSize * 2 * Screen.width / Screen.height, camera.orthographicSize * 2);
+        pixelSize = camWorldBoundry.x / resX;
+
+        visibleEnemies = new Dictionary<GameObject, LightTest>();
     }
-    
+
     void FixedUpdate()
     {
         moveDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
         rb.AddForce(moveDir.normalized * moveSpeed * 10.0f, ForceMode.Force);
     }
-    
+
+    private bool done = false;
     void Update()
     {
         // Temp flashlight testing
@@ -55,7 +74,50 @@ public class Player : MonoBehaviour
         {
             flashlight.SetActive(!flashlight.activeSelf);
         }
+        
+        Movement();
+        
+        Texture2D cameraRender = new Texture2D(resX, resY, TextureFormat.RGB24, false);
+        RenderTexture.active = camera.activeTexture;
+        cameraRender.ReadPixels(new Rect(0, 0, resX, resY), 0, 0);
+        cameraRender.Apply();
+        
+        foreach (KeyValuePair<GameObject, LightTest> enemy in visibleEnemies)
+        {
+            GameObject e = enemy.Key;
+                
+            Vector3 originScreenPos = camera.WorldToScreenPoint(e.transform.position);
+            originScreenPos.z = 0;
 
+            bool inLight = false;
+                
+            // Break if any pixel is in light?
+            for (int i = (int)originScreenPos.y - rayPaddingY; i < (int)originScreenPos.y + rayPaddingY; i++)
+            {
+                for (int j = (int)originScreenPos.x - rayPaddingX; j < (int)originScreenPos.x + rayPaddingX; j++)
+                {
+                        
+                    Vector3 rPos = camera.ScreenToWorldPoint(new Vector3(j, i, 0));
+                    rPos += camera.transform.right * pixelSize / 2;
+                    rPos += camera.transform.up * pixelSize / 2;
+                        
+                    // Debug.DrawLine(rPos, rPos + camera.transform.forward * 20);
+                        
+                    if (Physics.Raycast(rPos, camera.transform.forward, out var hitInfo, 100.0f,
+                            enemyMask))
+                    {
+                        if ((0.2126 * cameraRender.GetPixel(j, i).r + 0.7152 * cameraRender.GetPixel(j, i).g +
+                             0.0722 * cameraRender.GetPixel(j, i).b) > 0.01f) inLight = true;
+                    }
+                }
+            }
+
+            enemy.Value.inLight = inLight;
+        }
+    }
+
+    void Movement()
+    {
         // Mouse Pos * Resolution Scale to get accurate position
         mousePos = Input.mousePosition;
         mousePos.x *= resScaleX;
@@ -74,5 +136,15 @@ public class Player : MonoBehaviour
 
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    public void EnemyVisible(GameObject enemyObj, LightTest lt)
+    {
+        visibleEnemies.Add(enemyObj, lt);
+    }
+
+    public void EnemyLeftVisibility(GameObject enemyObj)
+    {
+        visibleEnemies.Remove(enemyObj);
     }
 }
